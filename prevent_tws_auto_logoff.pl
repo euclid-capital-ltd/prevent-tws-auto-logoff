7b742e1e87d19fb9775c3d4b3581d6bc0670af33
Sinisa Susnjar <sinisa.susnjar@gmail.com>
2019-02-07 15:35:17 +0000

    replaced tabs with blanks

--- prevent_tws_auto_logoff.pl
+++ prevent_tws_auto_logoff.pl
@@ -14,42 +14,42 @@ my $logoff_time = "11:59";
 my $verbose = 0;
 my $sleep = 10;
 
-GetOptions(	"timezone|tz=s"		=> \$tws_tz,
-			"logoff_time|time=s"=> \$logoff_time,
-			"verbose"			=> \$verbose,
-			"sleep=i"			=> \$sleep,
-			"help|?"			=> sub { pod2usage(1); exit(0); },
-			"man"				=> sub { pod2usage(-verbose => 2); exit(0); } ) or pod2usage(1);
+GetOptions( "timezone|tz=s"     => \$tws_tz,
+            "logoff_time|time=s"=> \$logoff_time,
+            "verbose"           => \$verbose,
+            "sleep=i"           => \$sleep,
+            "help|?"            => sub { pod2usage(1); exit(0); },
+            "man"               => sub { pod2usage(-verbose => 2); exit(0); } ) or pod2usage(1);
 
 while (42) {
-	foreach my $id (WaitWindowLike('Exit Session Setting', undef, 1)) {
-		if (IsWindowViewable($id)) {
-			my $now = DateTime->from_epoch(epoch => time(), time_zone => $tws_tz);
-			printf "$now: window #$id reset logoff time to $logoff_time " if $verbose;
-			ClickWindow($id, 125, 55);
-			SendKeys("^(a)");		# select all text
-			SendKeys($logoff_time);	# enter our preferred time
-			SendKeys("{TAB}");		# switch to AM/PM checkbox
-			if ($now->hour > 12) {
-				# it's afternoon, reset to AM
-				SendKeys("{SPC}");	# select AM checkbox
-				print "AM" if $verbose;
-			} else {
-				# it's morning, reset to PM
-				SendKeys("{RIG}");	# goto PM checkbox
-				SendKeys("{SPC}");	# select PM checkbox
-				print "PM" if $verbose;
-			}
-			print "\n" if $verbose;
-			# select "Update"
-			SendKeys("{TAB}");
-			SendKeys("{SPC}");
-			# select "Close"
-			SendKeys("{TAB}");
-			SendKeys("{SPC}");
-		}
-	}
-	sleep($sleep);
+    foreach my $id (WaitWindowLike('Exit Session Setting', undef, 1)) {
+        if (IsWindowViewable($id)) {
+            my $now = DateTime->from_epoch(epoch => time(), time_zone => $tws_tz);
+            printf "$now: window #$id reset logoff time to $logoff_time " if $verbose;
+            ClickWindow($id, 125, 55);
+            SendKeys("^(a)");        # select all text
+            SendKeys($logoff_time);  # enter our preferred time
+            SendKeys("{TAB}");       # switch to AM/PM checkbox
+            if ($now->hour > 12) {
+                # it's afternoon, reset to AM
+                SendKeys("{SPC}");   # select AM checkbox
+                print "AM" if $verbose;
+            } else {
+                # it's morning, reset to PM
+                SendKeys("{RIG}");   # goto PM checkbox
+                SendKeys("{SPC}");   # select PM checkbox
+                print "PM" if $verbose;
+            }
+            print "\n" if $verbose;
+            # select "Update"
+            SendKeys("{TAB}");
+            SendKeys("{SPC}");
+            # select "Close"
+            SendKeys("{TAB}");
+            SendKeys("{SPC}");
+        }
+    }
+    sleep($sleep);
 }
 
 __END__
